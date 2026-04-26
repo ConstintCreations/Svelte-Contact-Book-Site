4c438f72e84cf790c8c03f165891d084f636e792
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-26 10:40:01 -0500

    Added Tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    type Contact = {
+        name: string;
+        address: string;
+        email: string;
+        phone: string;
+    };
+
+    let contacts: Contact[] = [{name: 'Test', address: '123 Test St.', email: 'test@test.com', phone: '123-456-7890'}];
+</script>
+<main>
+    <div class="bg-olive-800">
+        <p>{contacts[0].name}</p>
+        <p>{contacts[0].address}</p>
+        <p>{contacts[0].email}</p>
+        <p>{contacts[0].phone}</p>
+    </div>
+    <div class="bg-olive-800">
+        +
+    </div>
+</main>
